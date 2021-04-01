6d995ebb47e60d68a79548db29a43f4adc7999fe
yarvinh <yarvinhhernandez@gmail.com>
2021-03-31 21:31:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+
+class Cat {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+ 
+    speak() {
+        return `${this.name} says meow!`;
+    }
+}
+class Dog {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+ 
+    speak() {
+        return `${this.name} says woof!`;
+    }
+}
+
+class Bird {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+ 
+    speak() {
+        if (this.name ==='Pablo'){
+           return`It's me! ${this.name}, the parrot!`
+        } else {
+           return `${this.name} says squawk!`
+        }
+    }
+}
